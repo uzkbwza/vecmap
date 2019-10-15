b7b80b574ed4d05c13bd2645ce68dd1b6e8e0642
Ian Sly <43023911+uzkbwza@users.noreply.github.com>
2019-10-15 12:44:18 -0700

    fixed "fix"

--- src/lib.rs
+++ src/lib.rs
@@ -74,13 +74,13 @@ impl<T> VecMap<T> where T: Clone + Copy {
     // a tile. Each row is stored sequentially (so 0..80, 81..160, etc.). This takes an x/y and returns
     // the array index.
     pub fn xy_idx(&self, x: i32, y: i32) -> usize{
-        let id = (x as usize * self.width as usize) + y as usize;
+        let id = (x as usize * self.height as usize) + y as usize;
         id
     }
 
     // It's a great idea to have a reverse mapping for these coordinates. This is as simple as
     // index % MAP_WIDTH (mod MAP_WIDTH), and index / MAP_WIDTH
     pub fn idx_xy(&self, idx: usize) -> (i32, i32) {
-        (idx as i32 % self.width, idx as i32 / self.width)
+        (idx as i32 % self.height, idx as i32 / self.height)
     }
 }
